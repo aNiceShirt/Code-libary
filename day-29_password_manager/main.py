ea0813638703ccde7a819b747bb8118ec9ff3b05
aNiceShirt <mathias.bhelnaes@gmail.com>
2024-03-14 12:17:49 +0100

    day29done

--- day-29_password_manager/main.py
+++ day-29_password_manager/main.py
@@ -1,5 +1,25 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = []
+
+    password_list += [random.choice(letters) for letter in range(random.randint(8, 10))]
+    password_list += [random.choice(symbols) for letter in range(random.randint(2, 4))]
+    password_list += [random.choice(numbers) for letter in range(random.randint(2, 4))]
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    return password
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -12,12 +32,18 @@ def get_entries():
 
 ## Saves login to a new line in txt and clears entires
 def save_login():
-    file = open("day-29_password_manager/data.txt", "a")
-    file.write(str(get_entries())+'\n')
-    file.close()
-    entry_website.delete(0,END)
-    #entry_email_username.delete(0,END)
-    entry_password.delete(0,END)
+    logininfo = get_entries()
+    if logininfo["website"] == "" or logininfo["username"] == "" or logininfo["password"] == "":
+        messagebox.showinfo(title="Error", message="Don't leave any fields empty")
+    else:
+        is_ok = messagebox.askokcancel(title=logininfo["website"], message = f"You entered: \n Email:{logininfo['username']} \n Password: {logininfo['password']} \n Is this ok to save?")
+        if is_ok:
+            file = open("day-29_password_manager/data.txt", "a")
+            file.write(str(logininfo) +'\n')
+            file.close()
+            entry_website.delete(0,END)
+            #entry_email_username.delete(0,END)
+            entry_password.delete(0,END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -44,20 +70,23 @@ label_password = Label(text="Password:", font=("calibri", 12, "bold"), bg="white
 label_password.grid(column=0, row=3)
 
 ## Entries ##
-entry_website = Entry(width=35, borderwidth=2)
+entry_website = Entry(width=38, borderwidth=2)
 entry_website.grid(column=1,row=1, columnspan=2)
 entry_website.focus()
 
-entry_email_username = Entry(width=35, borderwidth=2)
+entry_email_username = Entry(width=38, borderwidth=2)
 entry_email_username.grid(column=1,row=2, columnspan=2)
 entry_email_username.insert(0, "mhelnaes@gmail.com")
 
-entry_password = Entry(width=17, borderwidth=2,)
+entry_password = Entry(width=20, borderwidth=2)
 entry_password.grid(column=1,row=3, columnspan=1)
 
 ## Buttons ##
 def action_password():
-    print("Action_password")
+    password = generate_password()
+    entry_password.delete(0,END)
+    entry_password.insert(0, password)
+    pyperclip.copy(password)
 
 button = Button(text="Generate Password", command=action_password)
 button.grid(column=2, row=3, columnspan=1)
